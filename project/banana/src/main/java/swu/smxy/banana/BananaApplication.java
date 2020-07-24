90fadd0a3385008bdd723185393a549df29dfcba
JecosineMoc <mj_ol@outlook.com>
2020-07-25 03:13:41 +0800

    ADD:Add files
    Update readme and add backend demo

---
+++ project/banana/src/main/java/swu/smxy/banana/BananaApplication.java
@@ -0,0 +1,13 @@
+package swu.smxy.banana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BananaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BananaApplication.class, args);
+	}
+
+}
---
+++ project/banana/src/test/java/swu/smxy/banana/BananaApplicationTests.java
@@ -0,0 +1,13 @@
+package swu.smxy.banana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BananaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
