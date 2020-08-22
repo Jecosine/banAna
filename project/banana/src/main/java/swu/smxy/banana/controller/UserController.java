354e9602816f0021e2919a486ca52b65864e765e
JecosineMoc <mj_ol@outlook.com>
2020-08-22 23:17:38 +0800

    UPDATE:Test login [#4] [#6]

--- project/banana/src/main/java/swu/smxy/banana/controller/UserController.java
+++ project/banana/src/main/java/swu/smxy/banana/controller/UserController.java
@@ -1,11 +1,17 @@
 /*
  * @Date: 2020-08-22 01:37:06
  * @LastEditors: Jecosine
- * @LastEditTime: 2020-08-22 12:12:42
+ * @LastEditTime: 2020-08-22 23:16:23
  */
 package swu.smxy.banana.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,17 +22,32 @@ import swu.smxy.banana.entity.ResponseType;
 import swu.smxy.banana.entity.User;
 import swu.smxy.banana.service.UserService;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController extends BaseController<UserService>
 {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @RequestMapping(value = "/loginService", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseType<User> login(@RequestParam String userName, @RequestParam String password)
+    public ResponseType<User> loginService(@RequestParam String userName, @RequestParam String password)
     {
         return userService.loginService(userName, password);
+    }
+    
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    // @Controller
+    public String login(Model model, HttpServletRequest request, HttpServletResponse response)
+    {
+
+        User user = (User)request.getSession().getAttribute("user_auth");
+        if (user == null)
+        {
+            user = new User();
+            user.setUserName("jecosine");
+            request.getSession().setAttribute("user_auth", user);
+        }
+        return "login.html";
     }
 }
