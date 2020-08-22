9ce2600f9029b4717e4f314c3ebc182ae641a889
JecosineMoc <mj_ol@outlook.com>
2020-08-22 23:42:53 +0800

    UPDATE:Undo change return type [#6]

--- project/banana/src/main/java/swu/smxy/banana/service/UserService.java
+++ project/banana/src/main/java/swu/smxy/banana/service/UserService.java
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-07-25 17:00:29
  * @LastEditors: Jecosine
- * @LastEditTime: 2020-08-22 23:27:46
+ * @LastEditTime: 2020-08-22 23:42:16
  * @FilePath: \banana\src\main\java\swu\smxy\banana\service\UserService.java
  */
 package swu.smxy.banana.service;
@@ -24,24 +24,24 @@ public class UserService extends BaseService<User, UserMapper>
     private UserMapper mapper;
     @Resource(name = "sqlSessionFactory")
     private SqlSessionFactory sqlSessionFactory;
-    public User loginService(String userName, String password)
+    public ResponseType<User> loginService(String userName, String password)
     {
         mapper = sqlSessionFactory.openSession().getMapper(UserMapper.class);
         password = DigestUtils.md5DigestAsHex(password.getBytes());
         User user = mapper.getByNameAndPassword(userName, password);
-        // int status = 0;
-        // String message = "Login Successfully";
+        int status = 0;
+        String message = "Login Successfully";
         if(user == null)
         {
-            // status = -1;
-            // message = "Login failed, please check you username or password";
+            status = -1;
+            message = "Login failed, please check you username or password";
             System.out.println("Login failed");
         }
-        // ResponseType<User> response = new ResponseType<User>();
-        // response.setData(user);
-        // response.setStatus(status);
-        // response.setMessage(message);
-        return user;
+        ResponseType<User> response = new ResponseType<User>();
+        response.setData(user);
+        response.setStatus(status);
+        response.setMessage(message);
+        return response;
 
     }
 }
