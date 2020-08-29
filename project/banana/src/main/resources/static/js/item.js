03159c2b0fbba01bd94a83d3c4ac2182c3aa7f54
JecosineMoc <mj_ol@outlook.com>
2020-08-30 00:05:08 +0800

    UPDATE:Add detail title header and shop widget

--- project/banana/src/main/resources/static/js/item.js
+++ project/banana/src/main/resources/static/js/item.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-08-27 01:59:44
  * @LastEditors: Jecosine
- * @LastEditTime: 2020-08-29 21:34:34
+ * @LastEditTime: 2020-08-29 23:41:53
  */
 // var rdata = JSON.parse(regionData);
 var a = new Vue({
@@ -11,6 +11,7 @@ var a = new Vue({
     },
     data() {
         return {
+            shopRate: 4.5,
             qty: 0,
             typeOption: '',
             regionData:regionData,
