9bf96b633c67a0006475123f72d11ad445eb23fa
JecosineMoc <mj_ol@outlook.com>
2020-08-29 13:26:01 +0800

    UPDATE:Finish price and address style

--- project/banana/src/main/resources/static/js/item.js
+++ project/banana/src/main/resources/static/js/item.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-08-27 01:59:44
  * @LastEditors: Jecosine
- * @LastEditTime: 2020-08-29 12:04:49
+ * @LastEditTime: 2020-08-29 13:14:20
  */
 // var rdata = JSON.parse(regionData);
 var a = new Vue({
