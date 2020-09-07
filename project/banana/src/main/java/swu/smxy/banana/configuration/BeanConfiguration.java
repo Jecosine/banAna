e3ea5556ee9a7d68c870cba1015acc2702d06131
Jecosine <mj_ol@outlook.com>
2020-09-07 14:21:09 +0800

    ADD:Add costomized error page

--- project/banana/src/main/java/swu/smxy/banana/configuration/BeanConfiguration.java
+++ project/banana/src/main/java/swu/smxy/banana/configuration/BeanConfiguration.java
@@ -8,11 +8,15 @@ package swu.smxy.banana.configuration;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
@@ -47,5 +51,18 @@ public class BeanConfiguration
     public SqlSessionFactory getFactory()
     {
         return DBConnection.getFactory();
+    }
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer()
+    {
+        return new WebServerFactoryCustomizer<ConfigurableWebServerFactory>() {
+            @Override
+            public void customize(ConfigurableWebServerFactory factory) {
+                // factory.setPort(8081);
+                factory.addErrorPages(new ErrorPage(HttpStatus.BAD_REQUEST, "/400"));            
+                factory.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500"));            
+                factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404"));
+            }
+        };
     }
 }
