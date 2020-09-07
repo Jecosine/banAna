ffd6e7aeaa7d3dcea2ac103451b5d5f45ba95fd4
Jecosine <mj_ol@outlook.com>
2020-09-07 18:53:44 +0800

    UPDATE:Designed side menu style

--- project/banana/src/main/resources/static/js/personal.js
+++ project/banana/src/main/resources/static/js/personal.js
@@ -5,23 +5,56 @@ var a = new Vue({
   },
   data() {
     return  {
+      onTop: true,
+      screenWidth: window.innerWidth,
+      userData: {
+          'userName': 'jecosine',
+          'expired': new Date(),
+          'avatarUrl': '../static/img/avatar.jpg',
+          'address': "Chongqing",
+          'shopCart': {
 
+          }
+      },
+      activeIndex: '1',
+      activeIndex2: '1'
     }
   },
   methods: {
+    handleOpen: function()
+    {
 
+    },
+    handleClose: function()
+    {
+      
+    }
   },
   computed: {
 
   },
   watch: {
-
+    screenWidth(val) {
+      if (!this.timer)
+      {
+          this.screenWidth = val;
+          this.timer = true;
+          let that = this;
+          if(this.screenWidth > 1200)
+              this.isCollapsed = false;
+          setTimeout(() => {
+              // console.log(that.screenWidth);
+              that.timer = false;
+          }, 500);
+      }
+    }
   },
   mounted: function() {
     
   },
   created: function()
   {
+    var that = this;
     $.ajax({
       type: "get",
       cache: false,
